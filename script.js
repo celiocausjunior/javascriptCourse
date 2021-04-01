c56764a621efb7df6620146ccdb1d2fd32a3e7d8
Celio Caus Junior <celiocausjunior@gmail.com>
2021-04-01 20:05:24 -0300

    Lesson1

---
+++ script.js
@@ -0,0 +1,45 @@
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+
+function BMI(weight, height) {
+    return weight / height ** 2;
+}
+
+let markBmi = BMI(markWeight, markHeight);
+let johnBmi = BMI(johnWeight, johnHeight);
+
+function isHigher(markBmi, johnBmi) {
+    if (markBmi > johnBmi) {
+        return console.log(`"TEST1 -> Mark's BMI (${markBmi}) is higher than John's BMI (${johnBmi})"`)
+    } else {
+        return console.log(`"TEST1 -> Mark's BMI (${markBmi}) is NOT higher than John's BMI (${johnBmi})"`)
+    }
+}
+isHigher(markBmi, johnBmi);
+
+
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+
+
+function BMI(weight, height) {
+    return weight / height ** 2;
+}
+
+markBmi = BMI(markWeight, markHeight);
+johnBmi = BMI(johnWeight, johnHeight);
+
+function isHigher2(markBmi, johnBmi) {
+    if (markBmi > johnBmi) {
+        return console.log(`"TEST2 -> Mark's BMI (${markBmi}) is higher than John's BMI (${johnBmi})"`)
+    } else {
+        return console.log(`"TEST2 -> Mark's BMI (${markBmi}) is NOT higher than John's BMI (${johnBmi})"`)
+    }
+}
+
+isHigher2(markBmi, johnBmi);

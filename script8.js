24b5336b27419d9c2981bae4f879c87d872050cf
Celio Caus Junior <celiocausjunior@gmail.com>
2021-04-17 17:48:12 -0300

    Coding Challenge (Loops)

---
+++ script8.js
@@ -0,0 +1,27 @@
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const total = [];
+const billsLenght = bills.length;
+let sum = 0;
+let average = 0;
+
+function calcTip(bills) {
+
+    for (i = 0; i < billsLenght; i++) {
+        (bills[i] >= 50 && bills[i] <= 300 ? (tips[i] = bills[i] * 0.15) : (tips[i] = bills[i] * 0.20));
+        total[i] = bills[i] + tips[i];
+        console.log("Bill # " + [i + 1] + " Value: " + bills[i] + ". Tip = " + tips[i] + ". Total: " + total[i]);
+    }
+    calcAverage(total);
+}
+
+function calcAverage(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(`Average: ${(sum/arr.length)}`);
+}
+
+calcTip(bills);
+
+

2db7bedcd8b7f7c84e532b71fd3a4757edbdab80
Celio Caus Junior <celiocausjunior@gmail.com>
2021-04-27 23:10:15 -0300

    Creating a easy guess game

--- GuessMyNumber/starter/script.js
+++ GuessMyNumber/starter/script.js
@@ -11,13 +11,37 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
 
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-
-document.querySelector('.check').addEventListener('click', function(){
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = '⚠️ ☢️ Please, insert a number!';
 
-    if(!guess) {
-        document.querySelector('.message').textContent = 'Please, insert a number!'
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '👍🏼 😎 Correct Number!';
+
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📈 Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "You've lost the game! 😖 ";
+            document.querySelector('.score').textContent = 0;
+        }
+
+    } else {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📉 Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "You've lost the game! 😖 ";
+            document.querySelector('.score').textContent = 0;
+
+        }
     }
 });

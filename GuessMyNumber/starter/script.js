34e4fec4042ba074f89a7984aa9ef84a5ab6b7a9
Celio Caus Junior <celiocausjunior@gmail.com>
2021-05-01 16:30:24 -0300

    Highscore implementation

--- GuessMyNumber/starter/script.js
+++ GuessMyNumber/starter/script.js
@@ -13,10 +13,11 @@ console.log(document.querySelector('.guess').value); */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-        // when there is no input
+    // when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = '⚠️ ☢️ Digite um número!';
 
@@ -26,6 +27,11 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
         // when guess is too high
     } else if (guess > secretNumber) {

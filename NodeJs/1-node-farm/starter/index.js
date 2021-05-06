1ffafafa5a17085bae57799c15175d7a391683d3
Celio Caus Junior <celiocausjunior@gmail.com>
2021-05-06 17:24:01 -0300

    Routing and simple API

--- NodeJs/1-node-farm/starter/index.js
+++ NodeJs/1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-const { Http2ServerRequest } = require('http2');
+const url = require('url');
 
 
 ////////////////////////////////////
@@ -29,11 +29,32 @@ console.log('Will read file'); */
 
 ////////////////////////////////////
 //SERVER
+const data =fs.readFileSync(`./dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
 
-const server = http.createServer((req, res)=> {
-    res.end('Hello from the server!');
+
+const server = http.createServer((req, res) => {
+
+    const pathName = req.url;
+
+    if (pathName === '/overview' || pathName === '/') {
+        res.end('This is the OVERVIEW');
+
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+
+    } else if (pathName === '/api') {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data)
+    }
+     else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('<h1> Page not found </h1>');
+    }
 });
 
-server.listen(8000, 'localhost', ()=> {
+server.listen(8000, 'localhost', () => {
     console.log('Listening to request on port 8000');
-})
+});

f363ea8d350e57d715705bb0977fafc6a28a4d41
Celio Caus Junior <celiocausjunior@gmail.com>
2021-05-06 22:40:21 -0300

    New Game Button and Finish

--- JavaScript/PigGame/starter/script.js
+++ JavaScript/PigGame/starter/script.js
@@ -29,7 +29,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0]
+let scores = [0, 0]
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -66,7 +66,7 @@ btnHold.addEventListener('click', function () {
 
         //2. Check if player's score is >= 50
         // Finish the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 50) {
             playing = false;
             diceEl.classList.add('hidden');
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -75,6 +75,20 @@ btnHold.addEventListener('click', function () {
             //Switch to the next player
             switchPlayer();
         }
-    }
+    };
+});
 
+btnNew.addEventListener('click', function () {
+    scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    diceEl.classList.add('hidden');
+
+    for (let i = 0; i < scores.length; i++) {
+        document.querySelector(`.player--${i}`).classList.remove('player--winner');
+        document.querySelector(`.player--${i}`).classList.add('player--active');
+        document.getElementById(`score--${i}`).textContent = 0;
+        document.getElementById(`current--${i}`).textContent = 0;
+    }
 });

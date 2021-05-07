916aaad7e0841592f494faa9ef213b047ef8bd92
Celio Caus Junior <celiocausjunior@gmail.com>
2021-05-06 23:07:56 -0300

    End of Project

--- JavaScript/PigGame/starter/script.js
+++ JavaScript/PigGame/starter/script.js
@@ -11,6 +11,8 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+// initial variables
 let scores = [0, 0]
 let currentScore = 0;
 let activePlayer = 0;

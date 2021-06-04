7235bb2abf791b0d0ef982907a7ad23bea3b56eb
Celio Caus Junior <celiocausjunior@gmail.com>
2021-06-04 00:28:35 -0300

    Bind, Call and functions exercises

--- JavaScript/09-Data-Structures-Operators/starter/script.js
+++ JavaScript/09-Data-Structures-Operators/starter/script.js
@@ -194,20 +194,20 @@ GOOD LUCK 😀
 // Coding Challenge #4
 
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n');
-  console.log(rows);
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+//   console.log(rows);
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLocaleLowerCase().trim().split('_');
-    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
-    console.log(`${output.padEnd(20)}${'🙈'.repeat(i + 1)}`);
-  }
-});
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLocaleLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+//     console.log(`${output.padEnd(20)}${'🙈'.repeat(i + 1)}`);
+//   }
+// });
 
 
 
@@ -244,3 +244,25 @@ delayedDeparture    ✅✅✅✅✅
 // Afterwards, test with your own test data!
 
 // GOOD LUCK 😀 
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = function (str){
+  return str.slice(0,3).toUpperCase();
+}
+
+for (const flight of flights.split('+')){
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_', ' '
+    )} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(30);
+  console.log(output);
+}

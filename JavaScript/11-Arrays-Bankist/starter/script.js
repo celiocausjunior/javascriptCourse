b06c4a0dbb852574d3dfa9a7af2ced6740533ccc
Celio Caus Junior <celiocausjunior@gmail.com>
2021-06-19 22:42:11 -0300

    Bank Application

--- JavaScript/11-Arrays-Bankist/starter/script.js
+++ JavaScript/11-Arrays-Bankist/starter/script.js
@@ -151,8 +151,8 @@ const maxValue = movements.reduce((x, y) => {
 }, movements[0]);
 
 //Pipeline
-const totalDepositsUsd = movements.filter(mov => mov > 0).map(mov => mov * EurToUsd).reduce((x, y) => x + y, 0);
-console.log(totalDepositsUsd);
+// const totalDepositsUsd = movements.filter(mov => mov > 0).map(mov => mov * EurToUsd).reduce((x, y) => x + y, 0);
+// console.log(totalDepositsUsd);
 
 const updateUI = function (currentAccount) {
   //Display Movements
@@ -500,69 +500,158 @@ GOOD LUCK 😀
 
 //Flat Method
 
-const arr = [0, 1, [2, 3, 4], [5, 6], 7, 8, 9, 10]
-const arr2 = [0, 1, [2, [3, 4]], [5, 6], 7, [[8, 9]], 10]
+// const arr = [0, 1, [2, 3, 4], [5, 6], 7, 8, 9, 10]
+// const arr2 = [0, 1, [2, [3, 4]], [5, 6], 7, [[8, 9]], 10]
 
-console.log(arr.flat());
-console.log(arr2.flat(2));
+// console.log(arr.flat());
+// console.log(arr2.flat(2));
 
-const accountMovements = accounts.map((account) => account.movements);
-console.log(accountMovements);
-const arrayMovements = accountMovements.flat();
-console.log(arrayMovements);
+// const accountMovements = accounts.map((account) => account.movements);
+// console.log(accountMovements);
+// const arrayMovements = accountMovements.flat();
+// console.log(arrayMovements);
 
-const sumMovements = arrayMovements.reduce((x, y) => x + y, 0)
-console.log(sumMovements);
+// const sumMovements = arrayMovements.reduce((x, y) => x + y, 0)
+// console.log(sumMovements);
 
-//FlatMap Method
+// //FlatMap Method
 
-const sumMovements2 = accounts.flatMap((account) => account.movements).reduce((x, y) => x + y, 0);
-console.log(sumMovements2);
-
-
-const names = ['Celio', 'Geraldo', 'Eliezer', 'Julia', 'Marcos', 'Mauricio', 'Eric', 'Jonas', 'Carlos', 'Gabriela', 'Gabrielle', 'Clayton', 'Gersino', 'Pyetra', 'Evandro', 'Jean', 'Wesley', 'Valdenir', 'Munir', 'Gersino', 'Rodrigo', 'Gilberto', 'Renan', 'Jacqueline', 'Angelica', 'Ester', 'Joao', 'Rafael'];
+// const sumMovements2 = accounts.flatMap((account) => account.movements).reduce((x, y) => x + y, 0);
+// console.log(sumMovements2);
 
 
-//Sort mutates the array, but it works only in strings
-console.log(names.sort());
+// const names = ['Celio', 'Geraldo', 'Eliezer', 'Julia', 'Marcos', 'Mauricio', 'Eric', 'Jonas', 'Carlos', 'Gabriela', 'Gabrielle', 'Clayton', 'Gersino', 'Pyetra', 'Evandro', 'Jean', 'Wesley', 'Valdenir', 'Munir', 'Gersino', 'Rodrigo', 'Gilberto', 'Renan', 'Jacqueline', 'Angelica', 'Ester', 'Joao', 'Rafael'];
 
-//Numbers uses callback function
-movements.sort((x, y) => {
-  return (x > y ? 1 : -1);
+
+// //Sort mutates the array, but it works only in strings
+// console.log(names.sort());
+
+// //Numbers uses callback function
+// movements.sort((x, y) => {
+//   return (x > y ? 1 : -1);
+// });
+// console.log(movements);
+
+// //Numbers uses callback function (simplified function)
+// movements.sort((x, y) => y - x);
+
+// console.log(movements);
+
+
+// //create an Array
+// const x = new Array(7);
+// console.log(x);
+
+// x.fill(2, 0, 7);
+// console.log(x);
+
+
+// //Array.from
+
+// const y = Array.from({ length: 20 }, () => 1);
+// console.log(y);
+
+
+// //Dynamic Array
+// const z = Array.from({ length: 150 }, (_, y) => y + 1);
+// console.log(z);
+
+// //Random Array
+// const randomArray = Array.from({ length: 100 }, (_, __) => Math.floor(Math.random() * 101) + 1);
+// console.log(randomArray);
+
+// //Getting an element and add in array
+// labelBalance.addEventListener('click', function () {
+//   const movementsUI = Array.from(document.querySelectorAll('.movements__value'), (x) =>
+//     Number(x.textContent.replace('€', ' ')));
+//   console.log(movementsUI);
+// })
+
+// //Total deposits in Bank
+// const bankDepositsSum = accounts.flatMap((acc) => acc.movements).filter((dep) => dep > 0).reduce((x, y) => x + y, 0);
+// console.log(bankDepositsSum);
+
+
+// const numDepositsOneThousand = accounts.flatMap((account) => account.movements).filter((dep) => dep >= 1000).length;
+// console.log(numDepositsOneThousand);
+
+// const numDepositsOneThousand2 = accounts.flatMap((account) => account.movements).reduce((dep, mov) => (mov >= 1000 ? ++dep : dep), 0);
+// console.log(numDepositsOneThousand2);
+
+
+// const { deposits, withdrawals } = accounts.flatMap((account) => account.movements).reduce((acc, cur) => {
+//   // cur > 0 ? (acc.deposits += cur) : (acc.withdrawals += cur);
+//   acc[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+//   return acc;
+// },
+//   { deposits: 0, withdrawals: 0 });
+
+// console.log(deposits, withdrawals);
+
+// const convertTitleCase = function(title){
+//   const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+//   const titleCase = title
+//   .toLowerCase()
+//   .split(' ')
+//   .map((word)=> exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1))
+//   .join(' ');
+//   return titleCase;
+// };
+
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG title and a good example'));
+
+
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+
+//1.
+dogs.forEach((dog)=>{
+  dog.recommendedPortion = (Math.round(dog.weight ** 0.75 * 28));
 });
-console.log(movements);
+console.log(dogs);
 
-//Numbers uses callback function (simplified function)
-movements.sort((x, y) => y - x);
+//2.
 
-console.log(movements);
+const sarahsDog = dogs.filter((dog)=> dog.owners.includes('Sarah'));
+const curFoodSarahDog = sarahsDog.map(((dog)=> dog.curFood));
 
-
-//create an Array
-const x = new Array(7);
-console.log(x);
-
-x.fill(2,0,7);
-console.log(x);
+console.log(curFoodSarahDog);
 
 
-//Array.from
-
-const y = Array.from({length:20}, ()=>1);
-console.log(y);
-
-
-//Dynamic Array
-const z = Array.from({length:150}, (_,y)=> y+1);
-console.log(z);
-
-//Random Array
-const randomArray = Array.from({length:100}, (_,__)=> Math.floor(Math.random()*101)+1);
-console.log(randomArray);
-
-//Getting an element and add in array
-labelBalance.addEventListener('click', function(){
-  const movementsUI = Array.from(document.querySelectorAll('.movements__value'),(x)=>
-  Number(x.textContent.replace('€', ' ')));
-  console.log(movementsUI);
-})
